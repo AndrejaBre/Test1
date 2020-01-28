8bd14a79e59417f03cb974afe139b6343a857165
AndrejaZ <AndrejaZ@users.noreply.github.com>
2020-01-28 18:07:43 +0100

    Dodat style.css, debug.log, isto kao i doradjenja verzija index.html i app.js

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
-(function (){
-    console.log('radi iife');
-})();
-
-var a = prompt('Koliko puta zelite da stampate?');
-while (a > 50){
-    alert('Molim unesite roj manji od 50');
-    a = prompt('Unesite broj manji od 50');
+function godine(limit, ...god){
+    god.forEach(el => {
+        console.log(2020 - el >= limit);
+    });
 }
- stampaj(a);
 
-
-
-function stampaj(a){
-    for (let i = 0; i < a; i++){
-        console.log(`Ovo je ${i+1}. put`);
-    }
-}
+godine(18,1993, 2003, 2000, 2019);
