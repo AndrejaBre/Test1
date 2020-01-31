28a863a7bb004d31e74d8e7a8ec0ea2c3ca09cd0
AndrejaZ <AndrejaZ@users.noreply.github.com>
2020-01-31 17:44:03 +0100

    Test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-function godine(limit, ...god){
-    god.forEach(el => {
-        console.log(2020 - el >= limit);
-    });
+for (var i = 1; i<=5; i++){
+    (function(){
+       setTimeout(function timer(){
+           console.log(i);
+       }, i*1000); 
+    })();
 }
-
-godine(18,1993, 2003, 2000, 2019);
